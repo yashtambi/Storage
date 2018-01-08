fb559f69b55bfbbb0f602cc754c1352801600612
yash.tambi <yash.tambi@gmail.com>
2018-01-08 15:08:27 +0100

    1. Added model for biomass and used values as per Marieke's calculation

--- biomass/biomass_main.m
+++ biomass/biomass_main.m
@@ -1,14 +1,46 @@
+clear bioirradiation_var bioirradiation_var_1 loss index crops netpr region solarirradiation
+
 regions = 5;
+crops = 5;
 solarirradiation = simportfile('avg_irradiations.xlsx');
 t = 0.25:0.25:8760;     % time (in steps of 15 mins)
 
-solarirradiation = hourtoquarter(solarirradiation, t, 1);
+netpr = zeros(regions, 1);
 
-bioirradiation_var = zeros(regions, sizeof(t));
-tofuel = zeros(regions, sizeof(t));
-netpr = zeros(regions, sizeof(t));
-
-for i = 1:regions
-    [bioirradiation_var(i, :)] = bioirradiation(i, solarirradiation, t, 2976, 5304);
-    [tofuel(i, :), netpr(i, :)] = biomasstofuel(total, t, 0.025, 0.40, 889, 0.34, 5304, 0.05);
+if ~exist('biomassdata', 'var')
+    biomassdata_import;
 end
+
+
+%% All regions
+loss = 0.05;    % 5%
+
+
+% Areas for corn, soybeans, switchgrass, wood, algae (hectares)
+% respectively
+biodiesel_demand = 794;
+areas = [0, 6657084.7/3, 0, 0, 0;
+    0, 17811040.23/3, 0, 0, 0;
+    0, 1902024.2/3, 0, 0, 0;
+    0, 7394017.906/3, 0, 0, 2700000;
+    0, 100406.9681/3, 0, 0, 2700000;
+    ];
+
+waste_biodiesel = [2998800, 17564400, 3141600, 21491400, 5644161.676] / 1e6;
+
+for region = 1:regions
+    bioirradiation_var_1 = hourtoquarter(solarirradiation(:, region), t, 1);
+    for i = 1:crops
+        index = (crops*(i-1)) + region;
+        
+        bioirradiation_var = bioirradiation(1, bioirradiation_var_1, t, ...
+            biomassdata.Plantingtime(index), biomassdata.Harvestingtime(index));
+        
+        netpr(region) = netpr(region) + biomasstofuel(bioirradiation_var, t, biomassdata.Photosyntheticefficiency(index),...
+            biomassdata.Massratiokgproductkgplant(index), areas(i, region), ...
+            biomassdata.Ouputinputratiokgkg(index), biomassdata.Harvestingtime(index), loss);
+    end
+end
+
+netpr = netpr ./ 1e6; % Convert MWh to TWh
+biodiesel_total = sum(netpr) - sum(waste_biodiesel);
--- biomass/biomasstofuel.m
+++ biomass/biomasstofuel.m
@@ -1,16 +1,18 @@
-function [tofuel, netpr] = biomasstofuel(total, t, photo, mass, hectare, inputratio, endhour, loss)
-%{
+function [netpr, tofuel] = biomasstofuel(total, t, photo, mass, hectare, inputratio, endhour, loss)
+    %{
         photo = photosynthetic efficiency of the species (solar energy %
         which is saved in plant)
         mass = mass efficiency (kg of biofuel/plastic per kg of plant)
-        hectare = amount of hectares used 
+        hectare = amount of hectares used
         inputratio = yield in MWh at certain moment corrected for fuel use
         to make biofuel/plastic
         tofuel = calculated net energy content of the fuel in MWh at certain moment
-%}
-    y = total;
-    tofuel = y .* photo .* mass .* hectare * 10000 .* inputratio;
-    
+    %}
+    tofuel = total .* photo .* mass .* hectare * 10000 .* inputratio;
     z = tofuel(t == endhour);
     netpr = z .* (1-loss);
+    
+    if isempty(netpr)
+        netpr = 0;
+    end
 end

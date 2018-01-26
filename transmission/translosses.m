9f3803565e4816840cf396e13cae4a074820ad98
SukanyaPrabhudesai <34601117+SukanyaPrabhudesai@users.noreply.github.com>
2018-01-26 21:46:23 +0100

    clear files

--- transmission/translosses.m
+++
@@ -1,8 +0,0 @@
-function [ powerloss ] = translosses( demand, from, to, losscoeff, distmatrix )
-    %TRANSLOSSES Summary of this function goes here
-    %   Detailed explanation goes here
-    
-    dist = 
-    
-end
-
